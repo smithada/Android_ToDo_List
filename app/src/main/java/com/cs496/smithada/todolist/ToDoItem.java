4a2404abf18e7835ffce6c11e8c5dc45eeac01bc
Adam Smith <stupidalphabet@gmail.com>
2017-08-15 14:23:28 -0500

    added toolbar and action item to to_do_item view

--- app/src/main/java/com/cs496/smithada/todolist/ToDoItem.java
+++ app/src/main/java/com/cs496/smithada/todolist/ToDoItem.java
@@ -2,11 +2,16 @@ package com.cs496.smithada.todolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.ToggleButton;
+import android.view.MenuItem;
+
+import static com.cs496.smithada.todolist.R.styleable.MenuItem;
 
 public class ToDoItem extends AppCompatActivity {
 
@@ -25,17 +30,100 @@ public class ToDoItem extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_item);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
-        //get the first switch in this view
+        //get all the view options from the activity view
         repeatTask = (Switch) findViewById(R.id.switch_repeat_days);
+        todoItem = (EditText) findViewById(R.id.todo_item);
+        monday = (ToggleButton) findViewById(R.id.toggle_mon);
+        tuesday = (ToggleButton) findViewById(R.id.toggle_tues);
+        wednesday = (ToggleButton) findViewById(R.id.toggle_wed);
+        thursday = (ToggleButton) findViewById(R.id.toggle_thurs);
+        friday = (ToggleButton) findViewById(R.id.toggle_fri);
+        saturday = (ToggleButton) findViewById(R.id.toggle_sat);
+        sunday = (ToggleButton) findViewById(R.id.toggle_sun);
+        repeatWeekly = (Switch) findViewById(R.id.switch_repeat_weekly);
 
         //add a listener to the switch to toggle other options display
         repeatTask.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
-
                 handleOptionsDisplay(isChecked);
+
+                if (isChecked == true){
+                    monday.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+                        @Override
+                        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                            //if monday is checked
+                        }
+                    });
+                    tuesday.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+                        @Override
+                        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                            //if tuesday is checked
+                        }
+                    });
+                    wednesday.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+                        @Override
+                        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                            //if wed is checked
+                        }
+                    });
+                    thursday.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+                        @Override
+                        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                            //if thurs is checked
+                        }
+                    });
+                    friday.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+                        @Override
+                        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                            //if fri is checked
+                        }
+                    });
+                    saturday.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+                        @Override
+                        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                            //if sat is checked
+                        }
+                    });
+                    sunday.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+                        @Override
+                        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                            //if sun is checked
+                        }
+                    });
+                    repeatWeekly.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+                        @Override
+                        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                            //if repeat weekly is checked
+                        }
+                    });
+                }
             };
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.to_do_item_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.done:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     //if switch is checked show other stuff
